0c1156349680fc230bdbaf5c3a13d318bae55a7c
Nima Poshtiban <poshtibannima@gmail.com>
2021-06-27 15:54:22 +0430

    Table component implemented

--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
+  // adding custom style to table
+  @Input('class') classNames = '';
+  
+  @Input() data:{name:string,age:number,job:string}[] = [];
+  @Input() headers:{key:string,label:string}[] = [];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
